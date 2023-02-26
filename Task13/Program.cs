91c32967f8d7cfc17c29f15d32deb24f7e81fc67
Aoi <naztya2001@bk.ru>
2023-02-26 14:47:09 +0300

    Task13 done

---
+++ Task13/Program.cs
@@ -0,0 +1,19 @@
+﻿int IsConverted(){
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void CheckThree(int num){
+    if (num/100==0){
+        Console.WriteLine("Pick another number");
+    } else {
+        while(num/1000 != 0){ 
+        num=num/10;
+        }
+        num=num%10;
+        Console.WriteLine(num);
+    }
+}
+
+Console.WriteLine("Write a number");
+int number = IsConverted();
+CheckThree(number);

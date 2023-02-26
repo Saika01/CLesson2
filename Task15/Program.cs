208918e51094bc33c5bce02ee44098ff5c9ac254
Aoi <naztya2001@bk.ru>
2023-02-26 16:27:21 +0300

    Task15 done

---
+++ Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿int IsConverted(){
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Answer(int number){
+    if (number>7 || number<1) 
+        Console.Write("Pick another number");
+    else {
+        string[] arr = {"No", "No", "No", "No", "No", "Yes", "Yes"};
+        Console.WriteLine(arr[number-1]);
+    }
+}
+
+int digit = IsConverted();
+Answer(digit);
+
+

832e1c4b1a0b9b3cd16fe70075289f1e537fcc50
Aoi <naztya2001@bk.ru>
2023-02-26 14:11:17 +0300

    Task10 done

---
+++ Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿int IsConverted(){
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SecondDigit(int num){
+    return num/10%10;
+}
+
+Console.WriteLine("Write a number");
+int number = IsConverted();
+if (number/1000 == 0 && number/100 != 0) 
+    Console.WriteLine(SecondDigit(number));
+else
+    Console.WriteLine("Pick another number");
+
+

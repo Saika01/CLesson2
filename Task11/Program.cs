3f7c602d6afc74f8a9a5634f2013a4784286ebc5
Aoi <naztya2001@bk.ru>
2023-02-26 11:02:28 +0300

    task11

---
+++ Task11/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Random number from 100 to 999 -> {number}");
+int result = TheNumber(number);
+Console.WriteLine(result);
+
+int TheNumber(int num)
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    return firstDigit*10+thirdDigit;
+}

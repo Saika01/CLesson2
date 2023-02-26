b2f5fdec58e144cd914a557f1a3de7145b6a4be5
Aoi <naztya2001@bk.ru>
2023-02-26 10:42:04 +0300

    task09 done

---
+++ Task09/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine($"Random number from 10 to 99 -> {number}");
+int firstDigit = number/10;
+int secondDigit = number%10;
+if (firstDigit>secondDigit){
+    Console.WriteLine(firstDigit);
+} else if (secondDigit>firstDigit){
+    Console.WriteLine(secondDigit);
+} else {
+    Console.WriteLine("Errore");
+}
